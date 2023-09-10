55368b8f627755dfee0c26a24191d51df5d2997d
633230007 <144574821+633230007@users.noreply.github.com>
2023-09-10 13:22:39 +0700

    .commit

--- streamlit01.py
+++ streamlit01.py
@@ -5,9 +5,16 @@ st.title('My title ทดสอบด้วย streamlit')
 st.header("Theerakan 633230007")
 
 col1, col2 = st.columns(2)
-col1.write("This is column 1")
-col2.write("This is column 2")
+#col1.write("This is column 1")
+#col2.write("This is column 2")
 
+with col1:
+    st.header("TKKiT0007")
+    st.image("pic\iris1.jpg")
+
+with col2:
+    st.header("Versicolor")
+    st.image("")
 
 st.header('My header')
 st.subheader('My sub')
