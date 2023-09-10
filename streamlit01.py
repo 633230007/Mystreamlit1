9f59583e8d0d08931ead62418392e9ed90457952
633230007 <144574821+633230007@users.noreply.github.com>
2023-09-10 13:15:18 +0700

    .commit

--- streamlit01.py
+++ streamlit01.py
@@ -1,6 +1,13 @@
 import streamlit as st
 
-st.header("Theerakan 633230007")
 st.title('My title ทดสอบด้วย streamlit')
+
+st.header("Theerakan 633230007")
+
+col1, col2 = st.columns(2)
+col1.write("This is column 1")
+col2.write("This is column 2")
+
+
 st.header('My header')
 st.subheader('My sub')
