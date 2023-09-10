eb832242452badce96f6190ad121fe7573e8add8
633230007 <144574821+633230007@users.noreply.github.com>
2023-09-10 13:57:35 +0700

    .commit

--- streamlit01.py
+++ streamlit01.py
@@ -51,3 +51,10 @@ dt=pd.read_csv('./data/iris.csv')
 st.write(dt.head(10))
 
 st.button("showChart")
+
+dt1 = dt['petal.length'].sum()
+dt2 = dt['petal.width'].sum()
+dt3 = dt['sepal.length'].sum()
+dt4 = dt['sepal.width'].sum()
+dx = [dt1, dt2, dt3, dt4]
+dx2 = pd.DataFrame(dx, index=["d1", "d2", "d3", "d4"])
